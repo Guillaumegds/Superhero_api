bec1afd5eb096d0ab82bf1281870d2b560d25504
Guillaume de Stéfano <g.destefano@orange.fr>
2018-04-06 10:41:34 +0200

    fix url github

--- src/app/deploy.sh
+++ src/app/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 npm run build -- --base-href "https://guillaumegds.github.io/Superhero_api/"
-npm run deploy -- --repo="https://github.com/Guillaumegds/Superhero_api.git"
+npm run deploy -- --repo=https://github.com/Guillaumegds/Superhero_api.git
